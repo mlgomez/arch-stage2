962bb0355e45ac2b90f7d92c91599a1841038a10
mlgomez <miguel.leiva.gomez@gmail.com>
2021-12-05 03:34:45 +0200

    i'm new to this and it shows.....

--- stage2-default.sh
+++ stage2-default.sh
@@ -3,7 +3,7 @@
 #####################################################################
 # This is a personal installation script for stage 2 of archinstall #
 # Please run this only AFTER you have installed archlinux and       #
-# logged in. Do not run this as root or with chroot.                #
+# logged in.                                                        #
 #                                                                   #
 # When running archinstall on installation DO NOT install a         #
 # desktop environment. This script will install a window manager    #
@@ -31,24 +31,30 @@
 # Set some variables for this script
 workingdir=$PWD
 
-# Get the user's password
-echo "Please enter your user password to continue installation. Type 'fuckoff' to cancel."
-read -r passwd
-
-if [[ $passwd == "fuckoff" ]]; then
-	exit 0
-fi
-
 # Create typical subdirectories for home dir if they don't exist
 
-mkdir "$HOME/Downloads"
-mkdir "$HOME/Documents"
-mkdir "$HOME/Music"
-mkdir "$HOME/Pictures"
-mkdir "$HOME/Videos"
+if [[ !-d "$HOME/Downloads" ]]; then
+	mkdir "$HOME/Downloads"
+fi
+
+if [[ !-d "$HOME/Documents" ]]; then
+	mkdir "$HOME/Documents"
+fi
+
+if [[ !-d "$HOME/Music" ]]; then
+	mkdir "$HOME/Music"
+fi
+
+if [[ !-d "$HOME/Pictures" ]]; then
+	mkdir "$HOME/Pictures"
+fi
+
+if [[ !-d "$HOME/Videos" ]]; then
+	mkdir "$HOME/Videos"
+fi
 
 # Before running package management, update system
-echo "$passwd" | sudo pacman -Syu --noconfirm
+pacman -Syu --noconfirm
 
 #########################
 # Installing AUR Helper #
@@ -79,17 +85,17 @@ mkpkg -si --noconfirm
 #####################
 
 # Grab all the packages needed for graphics
-echo "$passwd" | sudo pacman -S --needed xorg lightdm
-echo "$passwd" | sudo systemctl enable lightdm
+pacman -S --needed xorg lightdm
+systemctl enable lightdm
 
 # Install i3-gaps and some other tools then copy configs
-echo "$passwd" | sudo pacman -S --needed --noconfirm i3-gaps feh \
+pacman -S --needed --noconfirm i3-gaps feh \
 fish fzf bat starship alacritty easyeffects neofetch \
 picom spectacle dunst polkit-kde-agend xorg-xrandr rofi plocate
 
 # Install yay pkgs
 
-echo "$passwd" | yay -S --needed --noconfirm polybar find-the-command exa
+yay -S --needed --noconfirm polybar find-the-command exa
 
 ######################
 # Configuring system #
@@ -100,14 +106,14 @@ echo "$passwd" | yay -S --needed --noconfirm polybar find-the-command exa
 mkdir "$HOME/.config"
 
 cd "$workingdir" || exit 1
-cp dotfiles/alacritty "$HOME/.config"
-cp dotfiles/easyeffects "$HOME/.config"
-cp dotfiles/fish "$HOME/.config"
-cp dotfiles/i3 "$HOME/.config"
-cp dotfiles/picom "$HOME/.config"
-cp dotfiles/polybar "$HOME/.config"
+cp -r dotfiles/alacritty "$HOME/.config"
+cp -r dotfiles/easyeffects "$HOME/.config"
+cp -r dotfiles/fish "$HOME/.config"
+cp -r dotfiles/i3 "$HOME/.config"
+cp -r dotfiles/picom "$HOME/.config"
+cp -r dotfiles/polybar "$HOME/.config"
 cp dotfiles/starship.toml "$HOME/.config"
-echo "$passwd" | sudo cp dotfiles/ftc.fish /usr/share/doc/find-the-command
+cp dotfiles/ftc.fish /usr/share/doc/find-the-command
 
 # Copy wallpapers and create wallpapers folder
 mkdir "$HOME/Wallpapers"
@@ -117,4 +123,4 @@ find . -iname '*.jpg' -exec cp {} "$HOME/Wallpapers" \;
 # Start services #
 ##################
 
-sudo systemctl start lightdm
+systemctl start lightdm
