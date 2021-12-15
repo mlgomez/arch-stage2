bf63715cf8fbf70ed3cb7f0723e59037d9002a39
mlgomez <miguel.leiva.gomez@gmail.com>
2021-12-15 18:23:44 +0200

    fixed weather, removed mpd

--- dotfiles/polybar/scripts/openweathermap-fullfeatured.sh
+++ dotfiles/polybar/scripts/openweathermap-fullfeatured.sh
@@ -21,7 +21,7 @@ get_icon() {
         *) icon="";
     esac
 
-    echo $icon
+    echo "$icon"
 }
 
 get_duration() {
@@ -92,5 +92,7 @@ if [ -n "$current" ] && [ -n "$forecast" ]; then
         daytime=" $(get_duration "$((sun_rise-now))")"
     fi
 
-    echo "$CITY $(get_icon "$current_icon") $current_temp$SYMBOL  $trend  $(get_icon "$forecast_icon") $forecast_temp$SYMBOL"
+    # Until I fix this mess, I'm commenting this out and putting in something simpler:
+    # echo "$CITY $(get_icon "$current_icon") $current_temp$SYMBOL  $trend  $(get_icon "$forecast_icon") $forecast_temp$SYMBOL"
+    echo " $CITY   ☁ : $current_temp$SYMBOL  ➡ : $forecast_temp$SYMBOL"
 fi
